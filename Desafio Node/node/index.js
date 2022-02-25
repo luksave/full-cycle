6382ae1eb0496dbcaac993a7901ba0958023dfaa
Lucas Felipe Moreira Silva <lucas.fmsilva@goias.gov.br>
2022-02-25 17:39:25 -0300

    ADD: Desafio Nodejs NGINX
    Desafio Full Cycle - Docker
    
    Utilização do NGINX como proxy reverso para execução de requisições ao nodejs

---
+++ Desafio Node/node/index.js
@@ -0,0 +1,51 @@
+const { json } = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(express.static("./"));
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: '123',
+    database:'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Lucas')`
+//connection.query(sql)
+connection.end()
+
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config);
+    const selection = `SELECT * FROM people`
+    
+    let resultsQuery = [];
+
+    var query = connection.query(selection, function (error, results, fields) {
+
+        if(results.length){
+            for(var i = 0; i<results.length; i++ ){
+                resultsQuery.push(results[i]);
+            }          
+        }
+ 
+        console.log(resultsQuery);
+
+        res.send('<h1>Full Cycle Rocks!!</h1>'
+                + JSON.stringify(resultsQuery))
+
+    });
+
+    connection.end()
+
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
---
+++ Desafio Node/node/runApp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script para forçar execução do node
+
+npm init -y
+
+npm install express --save 
+
+node index.js
